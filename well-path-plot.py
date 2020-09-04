12b95574bc7b2a7edc441f628178b719b83b10b9
Irene Wallis <irene@cubicearth.nz>
2020-09-04 14:58:51 +1200

    Inital commit

---
+++ well-path-plot.py
@@ -0,0 +1,10 @@
+# -----------------------------------------------------------
+# Demonstrates how to plot a well path in 3D 
+# Includes a method to plot data along that well path
+#
+# (C) 2020 Irene Wallis, Auckland, New Zealand 
+# Email: irene@cubicearth.nz
+# Released under a permissive open source licence Apache 2.0 
+# https://choosealicense.com/licenses/apache-2.0/
+# -----------------------------------------------------------
+
