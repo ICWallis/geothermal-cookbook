cca5d41cbe7dce77a285aa2a1a55fc11d87fb842
Irene Wallis <irene@cubicearth.nz>
2020-09-05 09:24:44 +1200

    Working version

--- well-path-plot.py
+++ well-path-plot.py
@@ -1,8 +1,5 @@
 # -----------------------------------------------------------
-# UNDER DEVELOPMENT: DO NOT USE
-# -----------------------------------------------------------
 # Demonstrates how to plot a well path in 3D 
-# Includes a method to plot data along that well path
 #
 # x is east, y is north, and z is total vertical depth (TVD)
 #
@@ -18,51 +15,43 @@ import pandas as pd
 import math
 
 import fractoolbox as ftb
+# https://github.com/ICWallis/fractoolbox
 
 #
 # Import well survey data
 #
 
 dfsurvey = pd.read_csv('testdata-survey.csv')
-print(dfsurvey.head(3))
+#print(dfsurvey.head(2))
 
 #
 # Define the casing shoe
 #
 
-# make a casing shoe df
 data = {
     'shoe': ['production casing shoe'],
     'depth_mMDRF': [1000],
     }
-casingshoe = pd.DataFrame(data)
+dfcasingshoe = pd.DataFrame(data)
 
-# interpolate xyz for casing shoe using fractoolbox.xyzinterp
-mDdat = casingshoe['depth_mMDRF']
+# Interpolate casing shoe xyz using fractoolbox.xyzinterp
+mDdat = dfcasingshoe['depth_mMDRF']
 mDsur = dfsurvey['depth_mMDRF']
 xsur = dfsurvey['easting_m']
 ysur = dfsurvey['northing_m']
 zsur = dfsurvey['TVD_mRF']
 dfxyz = ftb.xyzinterp(mDdat, mDsur, xsur, ysur, zsur) 
-dfxyz.columns = ['depth_mMDRFx','northing_m','easting_m','TVD_mRF']
-casingshoe = pd.concat([casingshoe,dfxyz], axis=1, join='inner')
-casingshoe = casingshoe.drop(['depth_mMDRFx'],1)
-
-# make df containing survey data for only the cased interval
-'''
-dfcased = dfsurvey[
-    (dfsurvey.depth_mMDRF < casingshoe) & 
-    (dfsurvey.depth_mMDRF > -0.1)
-    ]
-'''
+dfxyz.columns = ['depth_mMDRFx','easting_m','northing_m','TVD_mRF']
+dfcasingshoe = pd.concat([dfcasingshoe,dfxyz], axis=1, join='inner')
+dfcasingshoe = dfcasingshoe.drop(['depth_mMDRFx'],1)
+#print(dfsurvey.head(2))
 
 #
 # Work out the x & y limits for the plot
 #
-
 # To make the scale 1:1 for a single well:
 #   Take the min X and Y values and add the well terminal depth (mVD)
-
+#
 # To make the scale 1:1 for multiple wells:
 #   Take the min X and Y values and then add
 #   either the plot depth or width, whichever is greater.
@@ -79,7 +68,6 @@ Ymin = dfsurvey['northing_m'].min()
 Ymax = dfsurvey['northing_m'].max()
 print('Ymin =', Ymin,'Ymax =', Ymax,'\n')
 
-
 # Force the rounding direction
 def roundup(x):
     return int(math.ceil(x / 1000.0)) * 1000
@@ -92,70 +80,78 @@ YmaxR = roundup(Ymax)
 XminR = rounddown(Xmin)
 XmaxR = roundup(Xmax)
 
-Zshallowest = 0  # upperZlim use 0 if you want the collar at the top of the plot
-Zdeepest = dfsurvey['TVD_mRF'].max() # set to total vertical depth
+# Top of the plot: 
+#   use 0 if you want the rig floor at the top of the plot
+Zshallowest = 0
 
-# ---------
-# Make plot
-# ---------
+# Bottom of the plot: 
+#   set here to the well terminal depth but could be set deeper
+Zdeepest = dfsurvey['TVD_mRF'].max() 
 
+#
+# Plot the data
+#
+
+# Define the plot
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-# plot well path
+# Plot well path
 ax.plot(
     dfsurvey['easting_m'],
     dfsurvey['northing_m'],
     dfsurvey['TVD_mRF'],
-    color='#8c510a',
+    color='k',
     linewidth=1,
     )
 
-# plot casing shoe
+# Plot casing shoe
 ax.scatter(
-    casingshoe.iloc[0]['easting_m'],
-    casingshoe.iloc[0]['northing_m'],
-    casingshoe.iloc[0]['TVD_mRF'],
-    s=40, color='k')
+    dfcasingshoe.iloc[0]['easting_m'],
+    dfcasingshoe.iloc[0]['northing_m'],
+    dfcasingshoe.iloc[0]['TVD_mRF'],
+    s=40, color='k'
+    )
 
 #
 # Set axis limits
 #
+# Two options are provided for setting xy lims
+# Just commet out the one that you do not want to use
 
-# Two options for setting xy lims
-# ?????? describe
-
-# Set x,y axis to make nice tidy plot
+# Set to x,y axis and allow matplotlib to set the ratio
 #ax.set_xlim(XminR,XmaxR)
 #ax.set_ylim(YminR,YmaxR)
 
 
-# Set x,y axis to make 1:1 scale with the depth
-# and so the figure is centered on the production casing shoe
+# Set x,y axis to make the plot scale 1:1 (horozontal to vertical)
+# Arranged so the figure is centered on the collar (rig floor)
 ax.set_xlim(
-    casingshoe.iloc[0]['easting_m'] - Zdeepest / 2,
-    casingshoe.iloc[0]['easting_m'] + Zdeepest / 2
+    dfcasingshoe.iloc[0]['easting_m'] - Zdeepest / 2,
+    dfcasingshoe.iloc[0]['easting_m'] + Zdeepest / 2
     )
 
 ax.set_ylim(
-    casingshoe.iloc[0]['northing_m'] - Zdeepest / 2,
-    casingshoe.iloc[0]['northing_m'] + Zdeepest / 2
+    dfcasingshoe.iloc[0]['northing_m'] - Zdeepest / 2,
+    dfcasingshoe.iloc[0]['northing_m'] + Zdeepest / 2
     )
 
-# Well plots are in TVD so the Z axis argumnets are plotted
+# This well plot is in TVD so the Z axis argumnets are plotted
 # in the reverse of the cartesian coordiate system
 ax.set_zlim(Zdeepest,Zshallowest)
 
 #
 # Format plot
 # 
+# A rage of options for formatting the plot
+# Just comment out the ones you do not want to use
 
-# get rid of axis tick labels
+# Get rid of axis tick labels
 ax.xaxis.set_ticklabels([])
 ax.yaxis.set_ticklabels([])
-ax.zaxis.set_ticklabels([])
+#ax.zaxis.set_ticklabels([])
 
-# set axes as three lines
+# Set axes as three lines
 [t.set_va('center') for t in ax.get_yticklabels()]
 [t.set_ha('left') for t in ax.get_yticklabels()]
 [t.set_va('center') for t in ax.get_xticklabels()]
@@ -163,18 +159,20 @@ ax.zaxis.set_ticklabels([])
 [t.set_va('center') for t in ax.get_zticklabels()]
 [t.set_ha('left') for t in ax.get_zticklabels()]
 
-# make the panes transparent
+# Make the panes transparent
 ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
 
-# make the grid lines transparent
+# Make all the grid lines transparent
+#ax.grid(False)
+
+# Make one set of grid lines transparent
 ax.xaxis._axinfo["grid"]['color'] =  (1,1,1,0)
 ax.yaxis._axinfo["grid"]['color'] =  (1,1,1,0)
-ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
+#ax.zaxis._axinfo["grid"]['color'] =  (1,1,1,0)
 
-# turns the grid and fill off
-#ax.grid(False)
+# Turn the grid and fill off
 ax.xaxis.pane.set_edgecolor('black')
 ax.yaxis.pane.set_edgecolor('black')
 ax.xaxis.pane.fill = False
